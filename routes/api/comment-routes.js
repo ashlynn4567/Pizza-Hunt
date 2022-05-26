10de98a00e9320e832eac15f2540a235bd8301f8
Ashley's PC <ashlynn4567@gmail.com>
2022-05-26 00:10:56 -0500

    minor changes

---
+++ routes/api/comment-routes.js
@@ -0,0 +1,9 @@
+// imports
+const router = require("express").Router();
+const { addComment, removeComment } = require("../../controllers/comment-controller");
+
+// routes
+
+
+// exports
+module.exports = router;
