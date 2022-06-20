6930eab07e40d605060de733c167f0b05625cf58
Ashley's PC <ashlynn4567@gmail.com>
2022-06-20 18:26:49 -0500

    created comment reply schema

--- models/Comments.js
+++ models/Comments.js
@@ -1,18 +1,62 @@
 // imports
-const { Schema, model } = require("mongoose");
+const { Schema, model, Types } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+// create a reply schema 
+const ReplySchema = new Schema(
+    {
+        replyId: {
+            type: Schema.Types.ObjectId, 
+            default: () => new Types.ObjectId()
+        },
+        replyBody: {
+            type: String
+        },
+        writtenBy: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal = dateFormat(createdAtVal))
+        }
+    }, 
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
 
 // create the comment schema to regulate data rules
-const CommentSchema = new Schema({
-    writtenBy: {
-        type: String
-    },
-    commentBody: {
-        type: String
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
+const CommentSchema = new Schema(
+    {
+        writtenBy: {
+            type: String
+        },
+        commentBody: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal => dateFormat(createdAtVal))
+        }, 
+        // use ReplySchema to validate data for a reply
+        replies: [ReplySchema]    
+    }, 
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
     }
+);
+
+// get total count of comments and replies on retrieval as a virtual
+CommentSchema.virtual("replyCount").get(function() {
+    return this.replies.length;
 });
 
 // create the comment model using the comment schema
